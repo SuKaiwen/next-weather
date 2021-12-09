3141588df15c10d7475aee5562406cc25a1f1a64
Kevin Su <kevinsu@outlook.co.nz>
2021-12-09 18:42:31 +1300

    basic page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet"/>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp
